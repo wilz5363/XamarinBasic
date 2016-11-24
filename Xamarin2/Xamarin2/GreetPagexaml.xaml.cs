0042b565d36c4fa90069243ea188ed729e0bc037
Chan <wilson.chan@hpe.com>
2016-11-24 17:28:25 +0800

    Initial Commit

---
+++ Xamarin2/Xamarin2/GreetPagexaml.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Xamarin2
+{
+    public partial class GreetPagexaml : ContentPage
+    {
+        public List<String> Quotes = new List<string>(
+            new string[] {"String1", "String2", "String3", "String4", "String5", "String6" }        
+        );
+        public static int Index = 0;
+
+        public GreetPagexaml()
+        {
+            InitializeComponent();
+            Slider.Value = 16;
+            //Device.OnPlatform(
+            //     iOS: () =>
+            //     {
+            //         Padding = new Thickness(0, 20, 0, 0);
+            //     },
+            //    Android: () =>
+            //    {
+            //        Padding = new Thickness(10, 20, 0, 0);
+            //    }
+            // );
+        }
+
+        private void NextQuoteBtn_OnClicked(object sender, EventArgs e)
+        {
+
+            QuoteLbl.Text = Quotes[Index++ == Quotes.Count ? 0 : Index];
+        }
+    }
+}
